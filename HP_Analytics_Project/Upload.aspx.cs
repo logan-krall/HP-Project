fba84f2d1ff7f0bd4baf2ee787e2f89b8a6cad93
logan-krall <logan.krall@gmail.com>
2015-12-09 08:23:58 -0800

    3.03

--- HP_Analytics_Project/Upload.aspx.cs
+++ HP_Analytics_Project/Upload.aspx.cs
@@ -589,7 +589,8 @@ namespace HP_Analytics_Project.Images
             string file = "Spreadsheet" + DateTime.Now.ToString("yyyy-MM-dd--hh-mm-ss") + ".xls";
             name += file;
 
-            var fileStr = new FileInfo(name);
+            //var fileStr = new FileInfo(name);
+            var fileStr = new FileInfo(file);
 
             //Build a list of all acceptable data types for use in main loop type checking
             var dataTypes = new[] { typeof(Byte), typeof(SByte), typeof(Decimal), typeof(Double), typeof(Single), typeof(Int16), 
@@ -681,7 +682,8 @@ namespace HP_Analytics_Project.Images
 
             Response.ContentType = "application/vnd.ms-excel";
             Response.AppendHeader("Content-Disposition", "attachment; filename=" + file);
-            Response.TransmitFile(name);
+            //Response.TransmitFile(name);
+            Response.TransmitFile(file);
             Response.End();            
         }
     }
